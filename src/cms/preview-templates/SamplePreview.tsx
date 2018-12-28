4509ee3a55a5e036c9d08d4b1797e9262769cb40
Florian Hirsch <post@florianhirsch.de>
2018-12-28 17:07:20 +0100

    Remove console logging

--- src/cms/preview-templates/SamplePreview.tsx
+++ src/cms/preview-templates/SamplePreview.tsx
@@ -4,6 +4,5 @@ import Sample from '../../components/layout/Sample'
 
 export default ({ entry }: PreviewProps) => {
   const { body, ...data } = entry.getIn(['data']).toJS()
-  console.info(data)
   return <Sample {...data} html={body} contentIsMarkdown={true} />
 }
