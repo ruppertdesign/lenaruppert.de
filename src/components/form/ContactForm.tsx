b2cc423869dfddd0e485003c3bd47ba6eec12dba
Florian Hirsch <post@florianhirsch.de>
2018-12-30 18:17:50 +0100

    Submit contact form via JavaScript

--- src/components/form/ContactForm.tsx
+++ src/components/form/ContactForm.tsx
@@ -7,6 +7,8 @@ import HoneyPot from './HoneyPot'
 import styled from '@emotion/styled'
 import { Button } from './Buttons'
 import validator from './validator'
+import styleVars from '../../styles/styleVars'
+import { rhythm } from '../../utils/typography'
 
 interface Input {
   value: string
@@ -15,6 +17,7 @@ interface Input {
 
 interface State {
   novalidate: boolean
+  submitSuccess: boolean
   submitError: boolean
   name: Input
   email: Input
@@ -24,6 +27,11 @@ interface State {
 
 const Form = styled('form')`
   max-width: 22rem;
+`
+
+const Error = styled('div')`
+  margin: ${rhythm(1)} 0;
+  color: ${styleVars.colors.red};
 `
 
 const fieldNames = ['name', 'email', 'tel', 'message']
@@ -42,8 +50,15 @@ export default class ContactForm extends React.PureComponent<{}, State> {
   // @ts-ignore
   state: State = {
     novalidate: false,
+    submitSuccess: false,
     submitError: false,
     ...initialFormValues,
+  }
+
+  encode = data => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&')
   }
 
   handleSubmit = (event: React.FormEvent) => {
@@ -62,7 +77,21 @@ export default class ContactForm extends React.PureComponent<{}, State> {
     }
     const formValid = Object.values(fields).every(field => (field as any).valid)
     if (formValid) {
-      // TODO submit form
+      const body = Object.keys(fields).reduce(
+        (acc, curr) => ({
+          ...acc,
+          [curr]: fields[curr].value,
+        }),
+        { 'form-name': target.getAttribute('name') }
+      )
+      this.setState({ submitSuccess: true })
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode(body),
+      })
+        .then(() => this.setState({ submitSuccess: true }))
+        .catch(() => this.setState({ submitError: true }))
     } else {
       this.setState(fields)
     }
@@ -82,6 +111,7 @@ export default class ContactForm extends React.PureComponent<{}, State> {
   }
 
   render() {
+    const { submitSuccess, submitError, novalidate } = this.state
     return (
       <Form
         id="contactForm"
@@ -91,72 +121,83 @@ export default class ContactForm extends React.PureComponent<{}, State> {
         data-netlify="true"
         netlify-honeypot="fax"
         onSubmit={this.handleSubmit}
-        noValidate={this.state.novalidate}
+        noValidate={novalidate}
       >
         <Fieldset>
           <Legend>Kontakt</Legend>
 
-          {this.state.submitError && (
-            <div className="error" id="submitError">
+          {submitError && (
+            <Error>
               Leider konnten Ihre Daten nicht übertragen werden.
               <br />
               Bitte überprüfen Sie Ihre Daten oder versuchen es später noch
               einmal.
+            </Error>
+          )}
+
+          {submitSuccess && (
+            <div className="" id="submitSuccess">
+              Vielen Dank für Ihre Nachricht. Ich werde sie umgehend
+              beantworten.
             </div>
           )}
 
-          <InputText
-            id="name"
-            label="Name *"
-            value={this.state.name.value}
-            valid={this.state.name.valid}
-            onChange={this.handleUserInput}
-            required={true}
-            minLength={3}
-            maxLength={50}
-            pattern={validator.patterns.text}
-            errorMsg="Bitte geben Sie Ihren Namen ein"
-          />
+          {!submitSuccess && (
+            <React.Fragment>
+              <InputText
+                id="name"
+                label="Name *"
+                value={this.state.name.value}
+                valid={this.state.name.valid}
+                onChange={this.handleUserInput}
+                required={true}
+                minLength={3}
+                maxLength={50}
+                pattern={validator.patterns.text}
+                errorMsg="Bitte geben Sie Ihren Namen ein"
+              />
 
-          <InputText
-            id="email"
-            label="E-Mail *"
-            value={this.state.email.value}
-            valid={this.state.email.valid}
-            onChange={this.handleUserInput}
-            required={true}
-            maxLength={50}
-            pattern={validator.patterns.email}
-            errorMsg="Bitte geben Sie eine gültige E-Mail Adresse ein"
-          />
+              <InputText
+                id="email"
+                label="E-Mail *"
+                value={this.state.email.value}
+                valid={this.state.email.valid}
+                onChange={this.handleUserInput}
+                required={true}
+                maxLength={50}
+                pattern={validator.patterns.email}
+                errorMsg="Bitte geben Sie eine gültige E-Mail Adresse ein"
+              />
 
-          <InputText
-            id="tel"
-            label="Telefon"
-            value={this.state.tel.value}
-            valid={this.state.tel.valid}
-            onChange={this.handleUserInput}
-            maxLength={50}
-            pattern={validator.patterns.tel}
-            errorMsg="Bitte geben Sie nur gültige Zeichen ein: 0-9 / + - ( )"
-          />
+              <InputText
+                id="tel"
+                label="Telefon"
+                value={this.state.tel.value}
+                valid={this.state.tel.valid}
+                onChange={this.handleUserInput}
+                maxLength={50}
+                pattern={validator.patterns.tel}
+                errorMsg="Bitte geben Sie nur gültige Zeichen ein: 0-9 / + - ( )"
+              />
 
-          <InputTextArea
-            id="message"
-            label="Ihre Nachricht *"
-            value={this.state.message.value}
-            valid={this.state.message.valid}
-            onChange={this.handleUserInput}
-            required={true}
-            rows={6}
-            errorMsg="Bitte geben Sie Ihre Nachricht ein"
-          />
+              <InputTextArea
+                id="message"
+                label="Ihre Nachricht *"
+                value={this.state.message.value}
+                valid={this.state.message.valid}
+                onChange={this.handleUserInput}
+                required={true}
+                rows={6}
+                errorMsg="Bitte geben Sie Ihre Nachricht ein"
+              />
 
-          <HoneyPot name="fax" />
+              <HoneyPot name="fax" />
 
-          <Button id="submit" type="submit">
-            Senden
-          </Button>
+              <Button id="submit" type="submit">
+                Senden
+              </Button>
+            </React.Fragment>
+          )}
         </Fieldset>
       </Form>
     )
