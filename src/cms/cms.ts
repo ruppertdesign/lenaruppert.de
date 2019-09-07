ad7768b54e5aa0f36e0f398cf9dbb055b3f27621
Florian Hirsch <post@florianhirsch.de>
2019-09-07 16:36:41 +0200

    Switch from netlify-cms to netlify-cms-app

--- src/cms/cms.ts
+++ src/cms/cms.ts
@@ -1,8 +1,10 @@
-import CMS from 'netlify-cms'
+import CMS from 'netlify-cms-app'
 import PagePreview from './preview-templates/PagePreview'
 import SamplePreview from './preview-templates/SamplePreview'
 import typography from '../utils/typography'
 import './cms.css'
+
+CMS.init()
 
 // HACK: didn't find a good way to combine typography, our whole layout
 // and netlify cms preview. We're including the fonts via cms.css and
